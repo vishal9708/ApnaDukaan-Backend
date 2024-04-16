6addd70eb5591cec4de4cfe906ab2150d6caf8ec
vishal kumar <vishalraj42000@gmail.com>
2024-04-16 11:01:55 +0530

    initial commit

--- index.js
+++ index.js
@@ -8,14 +8,18 @@ const app = express();
 app.use(express.json())
 app.use(cors())
 
-mongoose.connect(process.env.MONGO_URI)
-.then((res) => {
-    console.log('mongodb connected')
-    app.use('/api', apiRoutes)
+
+app.get('/', (req, res) => {
+    res.send('hello world')
 })
-.catch((err) => {
-    console.log('err in mongo connection', err)
-})
+// mongoose.connect(process.env.MONGO_URI)
+// .then((res) => {
+//     console.log('mongodb connected')
+//     app.use('/api', apiRoutes)
+// })
+// .catch((err) => {
+//     console.log('err in mongo connection', err)
+// })
 
 
 app.listen(process.env.PORT, () => console.log(`running on port ${process.env.PORT}`))
